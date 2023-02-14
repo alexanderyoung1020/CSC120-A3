ad6b9ed38bffe7cff4168d8219115e46e61dc398
alexanderyoung1020 <118072569+alexanderyoung1020@users.noreply.github.com>
2023-02-13 23:39:43 -0500

    git config --global

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,129 @@
-class Conversation {
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
 
+
+class Conversation {
   public static void main(String[] arguments) {
-    // You will start the conversation here.
+    ArrayList <String> transcript = new ArrayList<>();
+    Scanner input = new Scanner(System.in); 
+    System.out.println("How many rounds will you like to do?"); 
+    transcript.add("How many rounds?");
+    int numOfRounds = input.nextInt();
+    System.out.println("Okay, lets do " + numOfRounds + "!");
+    int r = 0; 
+    
+   
+    String round = String.valueOf(numOfRounds);
+    transcript.add(round);
+    System.out.println("Hi there! What's on your mind?"); 
+    transcript.add("Hi there! What's on your mind?");
+    
+
+    
+    while(true){
+      Scanner input2 = new Scanner(System.in);
+      String chat = " ";
+      transcript.add(chat);
+    
+      String answer = input2.nextLine();
+      transcript.add(answer);
+      
+
+      String[] sentence = answer.split(" ");
+      int iterator = 0;
+      if (iterator == 0){
+          ArrayList <String> cannedResponses = new ArrayList<String>();
+          cannedResponses.add("Mhmm.");
+          cannedResponses.add("Sounds good!");
+          cannedResponses.add("That's so interesting.");
+          cannedResponses.add("Tell me more.");
+          cannedResponses.add("Wow!");
+
+
+          Random random = new Random(); 
+          int int_random = random.nextInt(5);
+          String response = cannedResponses.get(int_random);
+          // String response = cannedResponses[random.nextInt()];
+          // int x = random.nextInt(cannedResponses.length);
+          chat = " " + response;
+          iterator+=1;
+      
+    
+      for (int i=0; i < sentence.length; i++){
+        if (sentence[i].equals("I")){
+        sentence[i] = "you"; 
+        chat = chat + sentence[i] + " ";
+        iterator+=1;
+        }
+        else if (sentence[i].equals("me")){
+        sentence[i] = "you";
+        chat = chat + sentence[i] + " "; 
+        iterator+=1;
+        }
+        else if (sentence[i].equals("am")){
+        sentence[i] = "are"; 
+        chat = chat + sentence[i] + " "; 
+        iterator+=1;
+        }
+        else if (sentence[i].equals("you")){
+          sentence[i] = "I";
+          chat = chat + sentence[i]+ " "; 
+          iterator+=1;
+        }
+        else if (sentence[i].equals("my")){
+          sentence[i]="your";
+          chat = chat + sentence[i] + " ";
+          iterator+=1;
+        }
+        else if (sentence[i].equals("your")) {
+          sentence[i]="my";
+          chat = chat + sentence[i] + " ";
+          iterator+=1;
+        }
+        else{
+          chat+=sentence[i]+" ";
+        }
+
+        if(r==numOfRounds){
+          System.out.print("Thanks for chatting! Have a great day!");
+          System.out.print("Transcript: ");
+          for (int y =0; y < transcript.size(); y++){
+            String finalTranscript = " " + transcript.get(y);
+            System.out.print(finalTranscript);}
+            input.close();
+            input2.close();
+          break;
+
+          
+
+        }
+
+      }
+    }
+
+   
+    // r +=1; 
+    // System.out.print(chat);
+    // transcript.add(chat);
+
+    // if(r==numOfRounds){
+    //   System.out.print("Thanks for chatting! Have a great day!");
+    //   System.out.print("Transcript: ");
+    //   for (int x =0; x < transcript.size(); x++){
+    //     String finalTranscript = " " + transcript.get(x);
+    //     System.out.print(finalTranscript);}
+    //   input.close(); 
+      
+    //   break;
+    }
+
+    }
   }
-}
+
+
+
+
+      
+      
+  
--- reflection.md
+++ reflection.md
@@ -3,5 +3,7 @@ Use this file to record your reflection on this assignment.
 - Which classmates did you consult with while working on this assignment?
 - Which session(s) of TA / office hours did you attend?
 - What are your initial impressions of Java? 
+    Java requires intricate thinking and planning in order to execute the programs you wish to implement. Drawing out a map of what I wanted to achieve with my code helped me organize and strategize my thinking. 
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+Programming requires careful thought and discussion to take an idea that you will turn into a full fledged program that a user can interact with. 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
